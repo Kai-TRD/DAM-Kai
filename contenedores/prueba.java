60ae28aff12beb72b0d9e6157def148316e568aa
Kai-TRD <wkailuo03@gmail.com>
2023-03-21 14:37:55 +0100

    aaaaaaaa

---
+++ contenedores/prueba.java
@@ -0,0 +1,41 @@
+package contenedores;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.annotation.processing.SupportedOptions;
+import javax.swing.event.ListDataEvent;
+
+public class prueba {
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> listaNumeros = new ArrayList<Integer>();
+
+        HashSet<Integer> elementosSinRepetir = new HashSet <Integer>();
+        HashSet<Integer> elementosRepetidos = new HashSet <Integer>();
+        HashSet<Integer> elementosSoloUnaVez = new HashSet <Integer>();
+
+
+        for (int i = 0; i < 100; i++) {
+
+            int numero = (int)((Math.random()*10)+1);
+            listaNumeros.add(numero);
+        }
+
+        System.out.println(listaNumeros);
+
+        System.out.println("La lista tiene: " + listaNumeros.size() + " elementos");
+
+
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            if (listaNumeros.get(i) == 5) {
+                listaNumeros.remove(i);
+            }
+        }
+
+        System.out.println("La lista tiene: " + listaNumeros.size() + " elementos");
+
+    }
+
+
+}
