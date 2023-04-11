45a0c0cde2c2e8eab4d5305c30dd4e336e9f811b
Kai-TRD <wkailuo03@gmail.com>
2023-04-11 12:58:21 +0200

    A

--- Colecciones/ejercicios/ejercicio23.java
+++ Colecciones/ejercicios/ejercicio23.java
@@ -3,6 +3,16 @@ package Colecciones.ejercicios;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * En ajedrez, el valor de las piezas se mide en peones. Una dama vale 9 peones,
+ * una torre 5 peones, un alfil 3, un caballo 2 y un péon vale, lógicamente,
+ * 1 peón. Realiza un programa que genere al azar las capturas que ha hecho
+ * un jugador durante una partida. El número de capturas será un valor aleatorio
+ * entre 0 y 15. Hay que tener en cuenta que cada jugador tiene la posibilidad de
+ * capturar algunas de las siguientes piezas (no más): 1 dama, 2 torres, 2 alfiles,
+ * 2 caballos y 8 peones. Al final debe aparecer la puntuación total.
+ */
+
 public class ejercicio23 {
 
     public enum Tipo (DAMA, TORRE, ALFIL, CABALLO, PEON);
@@ -13,15 +23,7 @@ public class ejercicio23 {
         ArrayList<Tipo> piezas;
     
     }
-    /*
-     * En ajedrez, el valor de las piezas se mide en peones. Una dama vale 9 peones,
-     * una torre 5 peones, un alfil 3, un caballo 2 y un péon vale, lógicamente,
-     * 1 peón. Realiza un programa que genere al azar las capturas que ha hecho
-     * un jugador durante una partida. El número de capturas será un valor aleatorio
-     * entre 0 y 15. Hay que tener en cuenta que cada jugador tiene la posibilidad de
-     * capturar algunas de las siguientes piezas (no más): 1 dama, 2 torres, 2 alfiles,
-     * 2 caballos y 8 peones. Al final debe aparecer la puntuación total.
-     */
+
 
 
 
