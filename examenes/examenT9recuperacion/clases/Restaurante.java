cddf1c26549e275bb9852941e227d55782f03a89
Kai-TRD <wkailuo03@gmail.com>
2023-05-05 10:15:33 +0200

    SUSPENSO :(

---
+++ examenes/examenT9recuperacion/clases/Restaurante.java
@@ -0,0 +1,15 @@
+package examenes.examenT9recuperacion.clases;
+
+public class Restaurante {
+
+    public Carta getCarta() {
+        return null;
+    }
+
+    public void addProducto(int i, Producto producto) {
+    }
+
+    public void deleteProducto(int i, Producto producto) {
+    }
+    
+}
