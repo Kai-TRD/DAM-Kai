8fb14b7f1a6b252f231ed7c53f5ddb05b9b7b32e
Kai-TRD <wkailuo03@gmail.com>
2023-05-09 14:33:39 +0200

    a

--- pokemon_Kailuo/src/App.java
+++ pokemon_Kailuo/src/App.java
@@ -1,8 +1,39 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        /*
+         * Dado el fichero pokemon.json donde guardamos la información que se guarda en
+         * una pokedex (enciclopedia electrónica portátil que los entrenadores
+         * Pokémon llevan consigo para registrar automáticamente las fichas de
+         * todas las diversas especies Pokémon vistas y capturada)
+         * 
+         * Crea un proyecto llamado pokemon_TUNombre en el cual tendrás que implementar
+         * las clases Pokedex y Pokemon con los atributos necesarios, las clases para la
+         * lectura de un json y obtener la siguiente información
+         * 
+         * - Los 5 ultimos pokemons según el listado
+         *      
+         * - Datos de Pikachu
+         *      
+         * - Evolución de Charmander
+         *      
+         * - Pokemons de tipo fire
+         *      
+         * - Pokemons con debilidad water o electric (debilidad es la propiedad:
+         *      
+         * - weaknesses)
+         *      
+         * - Pokemon con mas debilidades
+         *      
+         * - Pokemon más alto
+         *
+         * Toda esta información la debes guardar en un fichero llamado
+         * consultasPokemon.txt
+         */
+
         System.out.println(" + ======== POKEDEX ======== + ");
 
-        System.out.println("prueba");
-        System.out.println("Hola");
+        
+
     }
 }
