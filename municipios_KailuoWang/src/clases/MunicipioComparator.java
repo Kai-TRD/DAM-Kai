47f9bcde16e9f8fbdedc3e335137d8c4062d3fbb
Kai-TRD <wkailuo03@gmail.com>
2023-05-15 19:03:36 +0200

    aaa

--- municipios_KailuoWang/src/clases/MunicipioComparator.java
+++ municipios_KailuoWang/src/clases/MunicipioComparator.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 public class MunicipioComparator implements Comparator<Municipio> {
 
     public int compare(Municipio municipio1, Municipio municipio2) {
-    
+
         int temp = municipio1.getMunicipios().compareTo(municipio2.getMunicipios());
-    
+
         if (temp != 0) {
             return temp;
         }
-        
+
         return Integer.compare(municipio1.getPeriodo(), municipio2.getPeriodo());
     }
 }
