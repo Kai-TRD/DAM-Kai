7b6bb79f256d1582318af52a589e30497272b23c
Kai-TRD <wkailuo03@gmail.com>
2023-05-19 00:32:14 +0200

    AA

--- carreraCiclista_Kailuo/src/App.java
+++ carreraCiclista_Kailuo/src/App.java
@@ -24,12 +24,19 @@ public class App {
         Carrera carrera = new Carrera(listaCiclistas, "Vuelta a Málaga");
 
         String contenido = "====NOMBRE DE LA CARRERA ====";
-            contenido += "\n====CICLISTAS PARTICIPANTES EN LA CARRERA ====";
-            for (int i = 0; i < listaCiclistas.size(); i++) {
-                System.out.println( listaCiclistas.get(i).toString());
-            }
+        contenido += "\n====CICLISTAS PARTICIPANTES EN LA CARRERA ====";
+        for (int i = 0; i < listaCiclistas.size(); i++) {
+            System.out.println(listaCiclistas.get(i).toString());
+        }
 
         ciclistaUtils.crearArchivoTexto("src/recursos/prueba.txt", contenido);
 
+        /*
+         * ====CICLISTAS PARTICIPANTES EN LA CARRERA ====
+         * ==== CLASIFICACIÓN FINAL ====
+         * ==== PODIUM ====
+         * ==== CLASIFICACIÓN POR EQUIPOS====
+         * ==== POSICIÓN DEL CORREDOR: Wilco Kelderman(BOHA) Dorsal: 22 ====
+         */
     }
 }
